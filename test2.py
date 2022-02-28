2f9c5938626860653f0d6697ccfe58b185bb9857
mincrt <rohmin@korea.kr>
2022-02-28 15:38:35 +0900

    test - fft function

--- test2.py
+++ test2.py
@@ -9,6 +9,6 @@ x = np.linspace(0.0, N*T, N)
 y = np.sin(60.0 * 2.0*np.pi*x) + 0.5*np.sin(90.0 * 2.0*np.pi*x)
 y_f = scipy.fft.fft(y)
 x_f = np.linspace(0.0, 1.0/(2.0*T), N//2)
-
-plt.plot(x_f, 2.0/N * np.abs(y_f[:N//2]))
+plt.plot(x,y,'b-')
+# plt.plot(x_f, 2.0/N * np.abs(y_f[:N//2]))
 plt.show()
