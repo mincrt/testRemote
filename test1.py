48913aee838ea41474cbcfde0a83c927b0123e51
mincrt <rohmin@korea.kr>
2022-03-10 18:02:38 +0900

    modified test1.py matplot learning...

--- test1.py
+++ test1.py
@@ -1,12 +1,30 @@
 import numpy as np
 from matplotlib import pyplot as plt
-import pandas as pd
+# import pandas as pd
 
 def mul(x,c):
     d = x * c
     return d
+def tellMe(a):
+    print(a[1])
 
-# def dist(xm,ym,xo,yo,dl,mod,mod0):
+x = np.arange(0, 2, 0.2)
+tellMe(x)
+
+
+# plt.plot(x, x, 'r--', x, x**2, 'bo', x, x**3, 'g-.')
+
+plt.plot(x, x, 'bo')
+plt.plot(x, x**2, color='#e35f62', marker='*', linewidth=2)
+plt.plot(x, x**4, color='forestgreen', marker='^', markersize=9)
+plt.grid(True, axis='both', color='red', alpha=0.1, linestyle='--')
+# plt.xticks([0, 1, 2])
+plt.xticks(np.arange(0,2.2,0.2))
+plt.yticks(np.arange(0, 13, 1))
+
+plt.show()
+
+    # def dist(xm,ym,xo,yo,dl,mod,mod0):
 #     ii = np.where((xm>xo-dl)&(xm<xo+dl))[0]
 #     jj = np.where((ym>yo-dl)&(ym<yo+dl))[0]
 #     H = [];    xx = xm[ii];    yy = ym[jj]
@@ -24,11 +42,4 @@ def mul(x,c):
 #     return H
 
 
-def tellMe(a):
-    if a == 1:
-        print("tell me!!!")
-
-for b in range(0,10):
-    a=1
-    tellMe(a)
 
